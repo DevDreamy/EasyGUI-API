e12542429877062b053c9f17a247711608128132
Leo <leonardo.rocha@samplemed.com.br>
2024-07-30 13:42:05 -0300

    fix: ensure thread has stopped before creating a new one

--- main.py
+++ main.py
@@ -11,30 +11,38 @@ from PyQt6.QtWidgets import (
     QComboBox,
     QLineEdit,
 )
-from PyQt6.QtCore import QThread
+from PyQt6.QtCore import QThread, pyqtSignal
 from flask import Flask, jsonify
+from werkzeug.serving import make_server
 
 
 class FlaskThread(QThread):
+    finished = pyqtSignal()
+
     def __init__(self):
         super().__init__()
         self.json_data = {}
         self.port = 4000
+        self._server = None
+        self._app = Flask(__name__)
 
-    def run(self):
-        app = Flask(__name__)
-
-        @app.route('/', methods=['GET'])
+        @self._app.route('/', methods=['GET'])
         def jsonResponse():
             return jsonify(self.json_data)
 
-        app.run(host='localhost', port=self.port)
+    def run(self):
+        self._server = make_server('localhost', self.port, self._app)
+        self._server.serve_forever()
 
     def update_json(self, new_json):
         self.json_data = new_json
 
     def update_port(self, port):
         self.port = port
+
+    def stop(self):
+        if self._server:
+            self._server.shutdown()
 
 
 class MainWindow(QWidget):
@@ -115,7 +123,8 @@ class MainWindow(QWidget):
             except json.JSONDecodeError:
                 QMessageBox.critical(self, 'Error', 'Invalid JSON format!')
         else:
-            self.flask_thread.terminate()
+            self.flask_thread.stop()
+            self.flask_thread.wait()
             self.flask_thread = FlaskThread()
             self.toggle_button.setText('Enable')
             self.json_enabled = False
