c2543ecf9dd2807d8b7ae76b88a21010fba48de7
Leo <leonardo.rocha@samplemed.com.br>
2024-07-31 16:03:40 -0300

    style: add blank lines

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from src.main_window import MainWindow
 
+
 def main():
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
