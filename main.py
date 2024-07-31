6aed4100f4f489ef7dd2d66ec8d59eee73845356
Leo <leozhaunr@yahoo.com.br>
2024-07-31 19:56:26 -0300

    chore: add app icon

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from src.main_window import MainWindow
 
 
 def main():
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icons/easy_gui_api_icon.ico"))
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec())
