d6a3fa377cd60d060dc4863347ac5e8e0f6a96d8
Leo <leonardo.rocha@samplemed.com.br>
2024-08-01 19:02:46 -0300

    feat: add english and portuguese translation files

---
+++ translations/en_US.ts
@@ -0,0 +1,120 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message>
+        <location filename="../src/main_window.py" line="101"/>
+        <source>Enter port (default is 4000)</source>
+        <translation>Enter port (default is 4000)</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="106"/>
+        <source>Authentication Type:</source>
+        <translation>Authentication Type:</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="135"/>
+        <source>JSON Response:</source>
+        <translation>JSON Response:</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="138"/>
+        <source>Use default JSON</source>
+        <translation>Use default JSON</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="139"/>
+        <source>Write your own JSON</source>
+        <translation>Write your own JSON</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="150"/>
+        <source>Import JSON File</source>
+        <translation>Import JSON File</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="156"/>
+        <source>Enter JSON here...</source>
+        <translation>Enter JSON here...</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="163"/>
+        <source>Start Server</source>
+        <translation>Start Server</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/main_window.py" line="372"/>
+        <source>Enter port (default is 4000)</source>
+        <translation>Enter port (default is 4000)</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="373"/>
+        <source>Authentication Type:</source>
+        <translation>Authentication Type:</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="374"/>
+        <source>JSON Response:</source>
+        <translation>JSON Response:</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="375"/>
+        <source>Use default JSON</source>
+        <translation>Use default JSON</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="376"/>
+        <source>Write your own JSON</source>
+        <translation>Write your own JSON</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="377"/>
+        <source>Import JSON File</source>
+        <translation>Import JSON File</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="378"/>
+        <source>Enter JSON here...</source>
+        <translation>Enter JSON here...</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="379"/>
+        <source>Start Server</source>
+        <translation>Start Server</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="382"/>
+        <source>None</source>
+        <translation>None</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="383"/>
+        <source>Basic Auth</source>
+        <translation>Basic Auth</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="384"/>
+        <source>JWT Bearer Auth</source>
+        <translation>JWT Bearer Auth</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="385"/>
+        <source>OAuth2</source>
+        <translation>OAuth2</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="386"/>
+        <source>API Key</source>
+        <translation>API Key</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.py" line="387"/>
+        <source>Digest</source>
+        <translation>Digest</translation>
+    </message>
+</context>
+</TS>
