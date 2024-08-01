2cf60dc66f5c79d5873e1c0fca58de1b14a90a84
Leo <leonardo.rocha@samplemed.com.br>
2024-08-01 19:00:03 -0300

    chore: delete unused files

--- src/ui_elements/json_input.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QTextEdit
-
-
-class JsonInput(QTextEdit):
-    def __init__(self):
-        super().__init__()
-        self.setPlaceholderText('Enter JSON here...')
--- src/ui_elements/port_input.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QLineEdit
-from PyQt5.QtGui import QIntValidator
-
-
-class PortInput(QLineEdit):
-    def __init__(self):
-        super().__init__()
-        self.setPlaceholderText('Enter port (default is 4000)')
-        self.setValidator(QIntValidator(1, 65535))
